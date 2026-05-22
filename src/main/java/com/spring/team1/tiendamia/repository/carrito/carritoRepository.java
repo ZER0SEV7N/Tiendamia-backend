9fe528d6416d35ce8ffe8bb728c98250b5f66ddc
ZER0SEV7N <danielenriquesinger0@gmail.com>
2026-05-22 00:21:15 -0500

    Feat: Creacion de mi rama

---
+++ src/main/java/com/spring/team1/tiendamia/repository/carrito/carritoRepository.java
@@ -0,0 +1,8 @@
+package com.spring.team1.tiendamia.repository.carrito;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.spring.team1.tiendamia.Models.Carrito.Carrito;
+
+public interface carritoRepository extends JpaRepository<Carrito, Long> {
+
+}
