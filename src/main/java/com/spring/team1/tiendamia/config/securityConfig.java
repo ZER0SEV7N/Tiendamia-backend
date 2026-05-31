dcd15630f7d2ed23e187e89b39eee0ddcca9cfa1
ZER0SEV7N <danielenriquesinger0@gmail.com>
2026-05-31 01:04:37 -0500

    merge

--- src/main/java/com/spring/team1/tiendamia/config/securityConfig.java
+++ src/main/java/com/spring/team1/tiendamia/config/securityConfig.java
@@ -64,7 +64,7 @@ public class securityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE", "OPTIONS", "PUT"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
 
