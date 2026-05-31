b64515771eb6ea15627c187d4ba60b55045aebd0
Giovanni Denis <1603456@senati.pe>
2026-05-31 11:24:15 -0500

    feat: crear el models Wishlist y DTO

--- src/main/java/com/spring/team1/tiendamia/models/wishlist/DTO/WishlistItemResponse.java
+++ src/main/java/com/spring/team1/tiendamia/models/wishlist/DTO/WishlistItemResponse.java
@@ -1,5 +1,13 @@
 package com.spring.team1.tiendamia.models.wishlist.DTO;
 
-public class WishlistItemResponse {
+import lombok.Data;
 
+@Data
+public class WishlistItemResponse {
+    private Integer idWishlist;
+    private Integer idProducto;
+    private String nombre;
+    private String imagenUrl;
+    private Double precio;
+    private Boolean enStock;
 }
--- src/main/java/com/spring/team1/tiendamia/models/wishlist/Wishlist.java
+++ src/main/java/com/spring/team1/tiendamia/models/wishlist/Wishlist.java
@@ -1,5 +1,35 @@
 package com.spring.team1.tiendamia.models.wishlist;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.spring.team1.tiendamia.models.productos.Productos;
+import com.spring.team1.tiendamia.models.usuario.Usuarios;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "wishlist", uniqueConstraints = {
+        @UniqueConstraint(columnNames = { "id_Usuario", "id_Producto" })
+})
+@Data
 public class Wishlist {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_Usuario", nullable = false)
+    private Usuarios usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_Producto", nullable = false)
+    private Productos producto;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private LocalDateTime createdAt;
 }
